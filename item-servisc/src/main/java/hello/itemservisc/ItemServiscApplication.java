f11c3e70a5e62cd9ee7ae9112e6e420d031f7145
YeongJae0114 <yjl8628@naver.com>
2024-01-19 12:02:51 +0900

    :tada: Sping-mvc
    project start

---
+++ item-servisc/src/main/java/hello/itemservisc/ItemServiscApplication.java
@@ -0,0 +1,13 @@
+package hello.itemservisc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiscApplication.class, args);
+	}
+
+}
---
+++ item-servisc/src/test/java/hello/itemservisc/ItemServiscApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.itemservisc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
